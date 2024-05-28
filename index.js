4087f5a84f9c4ad470f045d4437b2b76fd64aae8
HarshIyer <harshessi@gmail.com>
2024-05-28 19:09:11 +0530

    Revert breaking into arrays

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
 var crypto = require("crypto");
 var CryptoJS = require("crypto-js");
-var sha256 = require("crypto-js/sha256");
 
-function splitObject(obj) {
-  const keys = [];
-  const values = [];
-  for (const [key, value] of Object.entries(obj)) {
-    keys.push(key);
-    values.push(value);
-  }
-  return { keys, values };
-}
 
 function generateDerivedKey(key, salt) {
   return crypto.pbkdf2Sync(key, salt, 1000, 16, "sha256").toString("hex");
 }
 
-function combineArrays(keys, values) {
-  const obj = {};
-  if (keys.length !== values.length) {
-    throw new Error("The keys and values arrays must have the same length.");
-  }
-  for (let i = 0; i < keys.length; i++) {
-    obj[keys[i]] = values[i];
-  }
-  return obj;
-}
-
 function convertToAES(inputJSON, key) {
-  const { keys, values } = splitObject(inputJSON);
-  const minifiedJSONKeys = JSON.stringify(keys);
-  const minifiedJSONValues = JSON.stringify(values);
+
   const minifiedJSON = JSON.stringify({
-    minifiedJSONKeys,
-    minifiedJSONValues,
+    inputJSON,
   });
   const base64 = btoa(minifiedJSON);
   const salt = new crypto.randomBytes(20).toString("hex");
@@ -41,7 +17,7 @@ function convertToAES(inputJSON, key) {
   const derivedKey = generateDerivedKey(key, salt);
 
   const aesString = CryptoJS.AES.encrypt(base64, derivedKey).toString();
-  const sha256key = sha256(key).toString();
+  const sha256key = crypto.createHash("sha256").update(key).digest("hex");
   return {
     derivedKey,
     salt,
@@ -54,10 +30,7 @@ function convertFromAES(aesString, key, salt) {
   const derivedKey = generateDerivedKey(key, salt);
   const bytes = CryptoJS.AES.decrypt(aesString, derivedKey);
   const originalText = atob(bytes.toString(CryptoJS.enc.Utf8));
-  return combineArrays(
-    JSON.parse(JSON.parse(originalText).minifiedJSONKeys),
-    JSON.parse(JSON.parse(originalText).minifiedJSONValues)
-  );
+  return JSON.parse(originalText);
 }
 
 module.exports = {
